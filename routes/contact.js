6884ca7c037b97d350a854a0095c4e118c2bf5ac
nusratjt <njahan8@my.centennialcollege.ca>
2019-02-11 00:05:29 -0500

    added create functionality to the DB

--- routes/contact.js
+++ routes/contact.js
@@ -3,11 +3,11 @@ let router = express.Router();
 let mongoose = require('mongoose');
 
 // create a reference to the db schema
-let contact = require('../models/contact');
+let contactModel = require('../models/contact');
 
 /* GET Contact List page - READ Operation */
 router.get('/', (req, res, next) =>{
-    contact.find((err, contactList) => {
+    contactModel.find((err, contactList) => {
         if(err) {
             return console.error(err);
         }
@@ -21,6 +21,38 @@ router.get('/', (req, res, next) =>{
             
         }
     });
+});
+
+/* GEt Route for the add page
+this will display the add page */
+
+router.get('/add', (req,res,next) => {
+    res.render('contacts/add',{
+        title: 'Add New Contact'
+    });
+});
+
+
+// POST Route for processing the Add page
+router.post('/add', (req, res, next) => {
+    //console.log(req.body);
+    
+    let newContact = contactModel({
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
+
+    contactModel.create(newContact, (err, contactModel) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else {
+            //refresh hte contact list
+            res.redirect('/contact-list');
+        }
+    });  
 });
 
 module.exports = router;
